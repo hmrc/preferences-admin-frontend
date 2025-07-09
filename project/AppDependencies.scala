8678546638ff37ea2634f4dbc6aa29fd92fe19c1
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-07-09 12:16:48 +0100

    DC-7580 Update scoverage plugin (#113)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.14.0"
   
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "10.13.0",
-    "uk.gov.hmrc"   %% "play-partials-play-30"      % "10.0.0",
-    "org.typelevel" %% "cats-core"                  % "2.6.1"
+    "uk.gov.hmrc"   %% "play-partials-play-30"      % "10.1.0",
+    "org.typelevel" %% "cats-core"                  % "2.13.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"   %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.scalactic" %% "scalactic"              % "3.2.16"         % Test,
-    "org.jsoup"      % "jsoup"                  % "1.8.1"          % Test
+    "org.scalactic" %% "scalactic"              % "3.2.19"         % Test,
+    "org.jsoup"      % "jsoup"                  % "1.21.1"          % Test
   )
 }
--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -22,9 +22,9 @@ object ScoverageSettings {
   def apply(): Seq[Def.Setting[? >: String & Double & Boolean]] =
     Seq(
       // Semicolon-separated list of regexes matching classes to exclude
-      ScoverageKeys.coverageExcludedPackages := "<empty>;.*Reverse.*;.*(config|testonly).*;.*(BuildInfo|Routes).*",
-      ScoverageKeys.coverageMinimumStmtTotal := 81.27,
-      ScoverageKeys.coverageFailOnMinimum := false,
+      ScoverageKeys.coverageExcludedPackages := "<empty>;.*Reverse.*;.*(config|testonly).*;.*(BuildInfo|Routes).*;.*\\$anon.*",
+      ScoverageKeys.coverageMinimumStmtTotal := 73.00,
+      ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
       ConfigKey.configurationToKey(Test) / parallelExecution := false
     )
