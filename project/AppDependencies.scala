b41621696da0725812c7b72386b7ddca880a6794
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-07-24 14:13:35 +0100

    DC-7628 Update play-frontend-hmrc version for gov uk rebranding

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
   
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.3.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.7.0",
     "uk.gov.hmrc"   %% "play-partials-play-30"      % "10.1.0",
     "org.typelevel" %% "cats-core"                  % "2.13.0"
   )
