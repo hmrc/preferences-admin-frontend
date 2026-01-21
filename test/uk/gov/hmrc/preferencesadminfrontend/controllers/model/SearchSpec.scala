529adc9a3882fa1118a94e5fca72aa9ae455d260
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2026-01-21 12:41:58 +0000

    DC-8357: add tests for SearchSpec

--- test/uk/gov/hmrc/preferencesadminfrontend/controllers/model/SearchSpec.scala
+++ test/uk/gov/hmrc/preferencesadminfrontend/controllers/model/SearchSpec.scala
@@ -21,33 +21,28 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.libs.json.{ JsError, Json }
 import uk.gov.hmrc.preferencesadminfrontend.utils.SpecBase
 
-import java.time.{ ZoneId, ZonedDateTime }
+class SearchSpec extends PlaySpec with GuiceOneAppPerSuite with SpecBase {
 
-class EventSpec extends PlaySpec with GuiceOneAppPerSuite with SpecBase {
-
-  "Event" must {
+  "MultiSearch" must {
 
     "Serialize correctly in " in {
 
-      val event = Event(
-        eventType = "testEvent",
-        emailAddress = Some("test@email.com"),
-        timestamp = ZonedDateTime.of(2026, 1, 1, 1, 1, 1, 0, ZoneId.of("Europe/London")),
-        viaMobileApp = true
+      val multiSearch = MultiSearch(
+        identifiers = "identifier",
+        batch = "batch"
       )
 
-      val json = Json.toJson(event)
+      val json = Json.toJson(multiSearch)
 
       val expectedJson = Json.parse(
         """
                 {
-                  "eventType": "testEvent",
-                  "emailAddress": "test@email.com",
-                  "timestamp": "2026-01-01T01:01:01Z[Europe/London]",
-                  "viaMobileApp": true
+                  "identifiers": "identifier",
+                  "batch": "batch"
                 }
                 """
       )
+
       json mustBe expectedJson
 
     }
@@ -56,17 +51,16 @@ class EventSpec extends PlaySpec with GuiceOneAppPerSuite with SpecBase {
 
       val invalidJson = Json.parse(
         """
-                      {
-                        "emailAddress": "test@email.com",
-                        "timestamp": "2026-01-01T01:01:01Z[Europe/London]",
-                        "viaMobileApp": true
-                      }
-                      """
+                {
+                  "batch": "batch"
+                }
+                """
       )
 
-      invalidJson.validate[Event] mustBe a[JsError]
+      invalidJson.validate[MultiSearch] mustBe a[JsError]
 
     }
+
   }
 
 }
