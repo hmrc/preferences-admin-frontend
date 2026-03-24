07582aaa9f638bdb222b0f12dcf9f9798575d2e9
Tarek Ahmed <173894783+TarekAhmed-MG@users.noreply.github.com>
2026-03-24 10:01:39 +0000

    DC-8637: update bootstrapVersion to 10.7.0 (#144)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.6.0"
+  val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
