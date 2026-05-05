d021976d7c183afb6d1baad41633f27ef5b09af9
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2026-05-05 16:18:21 +0100

    DC-8817 Investigate file upload for preference misalignment issue fix

---
+++ app/uk/gov/hmrc/preferencesadminfrontend/controllers/UploadController.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.preferencesadminfrontend.controllers
+
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.scaladsl.*
+import org.apache.pekko.util.ByteString
+import play.api.Logging
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
+import play.api.mvc.*
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.preferencesadminfrontend.config.AppConfig
+import uk.gov.hmrc.preferencesadminfrontend.controllers.model.{ CsvData, SearchNinos }
+import uk.gov.hmrc.preferencesadminfrontend.services.*
+import uk.gov.hmrc.preferencesadminfrontend.views.html.*
+
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class UploadController @Inject() (
+  authorisedAction: AuthorisedAction,
+  uploadView: file_upload,
+  val mcc: MessagesControllerComponents
+)(implicit
+  appConfig: AppConfig,
+  ec: ExecutionContext,
+  actorSystem: ActorSystem
+) extends FrontendController(mcc) with I18nSupport with Logging with RoleAuthorisedAction(authorisedAction) {
+
+  override def role: Role = Role.Admin
+
+  val showUploadPage: Action[AnyContent] = authorisedAction { implicit request => _ =>
+    Future.successful(Ok(uploadView()))
+  }
+
+  def upload() = Action.async(parse.multipartFormData) { request =>
+    request.body
+      .file("csvFile")
+      .map { filePart =>
+        val path = filePart.ref.path
+
+        // Create a stream: File -> Lines -> CsvData -> API Call
+        val source = FileIO
+          .fromPath(path)
+          .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 1024, allowTruncation = true))
+          .map(_.utf8String.trim)
+          .filter(_.nonEmpty)
+          .map { line =>
+            val cols = line.split(",").map(_.trim)
+            if (cols.length >= 3) Some(CsvData(cols(0), cols(1), cols(2))) else None
+          }
+          .collect { case Some(data) => data }
+
+        source
+          .mapAsync(4) { record =>
+            val jsonPayload = Json.toJson(record)
+            println(s"$jsonPayload")
+            Future.successful("jsonpayload to be posted")
+          }
+          .runWith(Sink.ignore)
+          .map { _ =>
+            Ok("Processing complete.")
+          }
+          .recover { case e: Exception =>
+            InternalServerError(s"Error: ${e.getMessage}")
+          }
+
+      }
+      .getOrElse {
+        Future.successful(BadRequest("File missing!"))
+      }
+  }
+}
