d92cd12dd09481f219df7f2d0a1b4d93ce10199d
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2026-07-24 18:00:32 +0100

    [DC-9225] Add check for bulk opt outs in AuthorisedAction

--- app/uk/gov/hmrc/preferencesadminfrontend/controllers/AuthorisedAction.scala
+++ app/uk/gov/hmrc/preferencesadminfrontend/controllers/AuthorisedAction.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.preferencesadminfrontend.controllers
 
-import play.api.mvc.{ Action, AnyContent, MessagesBaseController, MessagesControllerComponents, Request, Result }
+import play.api.mvc.{ Action, AnyContent, MessagesBaseController, MessagesControllerComponents, MessagesRequest, Request, Result }
 import uk.gov.hmrc.preferencesadminfrontend.controllers.Role.Generic
 import uk.gov.hmrc.preferencesadminfrontend.controllers.model.User
 import uk.gov.hmrc.preferencesadminfrontend.services.LoginService
@@ -29,14 +29,33 @@ class AuthorisedAction @Inject() (loginService: LoginService, val controllerComp
 
   def async(block: Request[AnyContent] => User => Future[Result]): Action[AnyContent] = async(Generic)(block)
 
-  def async(role: Role)(block: Request[AnyContent] => User => Future[Result]): Action[AnyContent] =
+  def async(role: Role)(
+    block: Request[AnyContent] => User => Future[Result]
+  ): Action[AnyContent] =
     Action.async { implicit request =>
       val isAdmin = request.session.get("isAdmin").getOrElse("false").toBoolean
       val user = request.session.get(User.sessionKey).map(name => User(name, ""))
 
       user match {
-        case Some(user) if loginService.hasRequiredRole(user, role) || isAdmin => block(request)(user)
+        case Some(user) if checkForRequiredRoleAndBulkOptOutsAccess(user, role) || isAdmin => block(request)(user)
         case _ => Future.successful(play.api.mvc.Results.Redirect(routes.LoginController.showLoginPage()))
       }
     }
+
+  private def checkForRequiredRoleAndBulkOptOutsAccess(user: User, role: Role)(implicit
+    request: MessagesRequest[AnyContent]
+  ) =
+    if (request.uri.equals(routes.CsvUploadBulkOptOutsController.showBulkOptOutsUploadPage.url)) {
+      loginService.hasRequiredRole(user, role) && checkAccessForCsvUploadBulkOptOuts
+    } else {
+      loginService.hasRequiredRole(user, role)
+    }
+
+  private def checkAccessForCsvUploadBulkOptOuts(implicit request: MessagesRequest[AnyContent]): Boolean = {
+    val isAdmin = request.session.get("isAdmin").getOrElse("false").toBoolean
+    val isGeneric = request.session.get("isGeneric").getOrElse("false").toBoolean
+    val isSols = request.session.get("isSols").getOrElse("false").toBoolean
+
+    (isGeneric || isAdmin) && (!isSols)
+  }
 }
